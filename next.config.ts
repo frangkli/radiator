895ba3c0b92d3e3c4c0f99fd619efce270dec1b2
Frank Li <frankla0203@outlook.com>
2025-03-27 20:14:50 -0700

    build: setup biome and frontend environment

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,13 @@ const isProd = process.env.NODE_ENV === "production";
 const internalHost = process.env.TAURI_DEV_HOST || "localhost";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  distDir: "build",
-  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
+	reactStrictMode: true,
+	output: "export",
+	images: {
+		unoptimized: true,
+	},
+	distDir: "build",
+	assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
