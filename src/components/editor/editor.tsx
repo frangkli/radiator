6cbaba625d8cf20719289550534c5eb59a6c7d1b
Frank Li <frankla0203@outlook.com>
2025-03-30 18:07:26 -0700

    feat: create skeleton for main page with sidebar

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EditorProvider, FloatingMenu, BubbleMenu } from "@tiptap/react";
+import { BubbleMenu, EditorProvider, FloatingMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 const extensions = [StarterKit];
@@ -8,7 +8,16 @@ const content = "<p>Hello World</p>";
 
 export function TipTapProvider() {
 	return (
-		<EditorProvider extensions={extensions} content={content}>
+		<EditorProvider
+			extensions={extensions}
+			content={content}
+			autofocus
+			editorProps={{
+				attributes: {
+					class: "min-h-full min-w-full outline-none",
+				},
+			}}
+		>
 			<FloatingMenu editor={null}>This is the floating menu</FloatingMenu>
 			<BubbleMenu editor={null}>This is the bubble menu</BubbleMenu>
 		</EditorProvider>
